e7405c5769d2eeb62b8ac62de45c5545fb780af0
khanni1 <khanjanc.pro@gmail.com>
2026-07-22 23:05:35 +0530

    some organise changes

--- add_commit_push_auto.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set /p branch=ENTER BRANCH TO PUSH:
-set /p msg=ENTER COMMIT MESSAGE: 
-
-for /f %%A in ('git remote') do set remote=%%A
-
-git add .
-git commit -m "%msg%"
-git push "%remote%" "%branch%"
